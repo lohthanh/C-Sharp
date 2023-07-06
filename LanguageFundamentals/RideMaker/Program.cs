ca2b078f9c7b71402bdc92990521595c074713ec
Thanh Lo <116846461+lohthanh@users.noreply.github.com>
2023-07-05 19:24:26 -0700

    create vehicle class and constructors

---
+++ LanguageFundamentals/RideMaker/Program.cs
@@ -0,0 +1,20 @@
+﻿Vehicle HouseCar = new Vehicle("Mazda", 4, "Blue", true);
+Vehicle Bus = new Vehicle("Hound", 25, "White", true);
+Vehicle MyBike = new Vehicle("Bike", 1, "Black", false);
+Vehicle MiniCoup = new Vehicle("Coup", "Red");
+
+List<Vehicle> VehicleList = new List<Vehicle>();
+
+VehicleList.Add(HouseCar);
+VehicleList.Add(Bus);
+VehicleList.Add(MyBike);
+VehicleList.Add(MiniCoup);
+
+// foreach (Vehicle car in VehicleList)
+// {     
+//     car.ShowInfo();
+// }
+
+MiniCoup.Travel(100);
+MiniCoup.ShowInfo();
+MiniCoup.Travel(50);
---
+++ LanguageFundamentals/RideMaker/Vehicle.cs
@@ -0,0 +1,40 @@
+class Vehicle
+{
+    public string Name;
+    public int NumOfPassengers;
+    public string Color;
+    public bool HasEngine;
+    public int DistanceTraveled;
+
+    public Vehicle(string name, int numOfPassengers, string color, bool hasEngine)
+    {   
+        Name = name;
+        NumOfPassengers = numOfPassengers;
+        Color = color;
+        HasEngine = hasEngine;
+        DistanceTraveled = 0;
+    }
+    public Vehicle(string name, string color)
+    {   
+        Name = name;
+        Color = color;
+        NumOfPassengers = 4;
+        HasEngine = true;
+        DistanceTraveled = 0;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Number of passengers: {NumOfPassengers}");
+        Console.WriteLine($"Color: {Color}");
+        Console.WriteLine($"Has engine: {HasEngine}");
+        Console.WriteLine($"Distance traveled: {DistanceTraveled}");
+    }
+
+    public void Travel(int distance)
+    {
+        DistanceTraveled += distance;
+        Console.WriteLine($"Total distance traveled: {DistanceTraveled}");
+    }
+}
