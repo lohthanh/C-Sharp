f2512e372835aacf8e1eef622f29129089be3167
Thanh Lo <116846461+lohthanh@users.noreply.github.com>
2023-07-07 19:14:36 -0700

    created first web app

---
+++ ASP-NET/PortfolioI/Controllers/PortfolioController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Portfolio.Controllers;
+
+public class PortfolioController : Controller
+{
+    [HttpGet]
+    [Route("")]
+    public string Index()
+    {
+        return "This is my Index!";
+    }
+
+    [HttpGet("projects")]
+    public string Projects()
+    {
+        return "These are my projects";
+    }
+
+    [HttpGet("contact")]
+    public string Contact()
+    {
+        return "This is my Contact!";
+    }
+
+}
