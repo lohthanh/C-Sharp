4e108d82f56b1b04def28d58545fe0cd05eadbd7
Thanh Lo <116846461+lohthanh@users.noreply.github.com>
2023-07-07 20:09:10 -0700

    Rendering views and razor in cshtml

---
+++ ASP-NET/RazorFun/Controllers/RazorFunController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun.Controllers;
+
+public class RazorFunController : Controller
+{
+    [HttpGet]
+    [Route("")]
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+
+}
